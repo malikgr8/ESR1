bed5f3f7ec958f02190f4dc829cfc6ea398c3431
malikgr8 <ahmad.cuteboy98@gmail.com>
2021-04-15 23:25:11 +0500

    image scene

--- project/urls.py
+++ project/urls.py
@@ -24,8 +24,9 @@ mypatterns = [
 ]
 urlpatterns = [
     path('backend/', include(mypatterns)),
-]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
 
 #if not settings.DEBUG:
-    #urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
