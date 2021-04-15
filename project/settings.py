a79e99b9668abc75a71cf702aea5e9acba62532b
malikgr8 <ahmad.cuteboy98@gmail.com>
2021-04-16 00:02:59 +0500

    image scene

--- project/settings.py
+++ project/settings.py
@@ -182,7 +182,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 
-STATIC_URL = '/static-files/'
+STATIC_URL = '/staticfiles/'
 #STATIC_ROOT = '/static-files'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
