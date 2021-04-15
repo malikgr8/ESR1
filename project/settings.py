3e8a4191fe7bcbe1a6277df88aee3e7ee0b1999c
malikgr8 <ahmad.cuteboy98@gmail.com>
2021-04-15 23:07:42 +0500

    image scene

--- project/settings.py
+++ project/settings.py
@@ -197,8 +197,8 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'data') # 'data' is my media folder
 
 STATICFILES_DIRS = [
-    STATIC_ROOT,
-    MEDIA_ROOT
+    BASE_DIR / 'static',
+    BASE_DIR / 'data'
 ]
 
 
